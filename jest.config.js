8d49c0ef6996b54854ce68a46480fbc52090b445
Nacho Langdon <nacholangdon@gmail.com>
2023-03-23 12:31:02 -0300

    feat(jest): Add jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+import type { Config } from 'jest';
+
+const jestConfig: Config = {
+  // ...other options
+  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
+};
+
+export default jestConfig;
